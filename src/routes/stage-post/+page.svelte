17813e3cddb4e2a39aa97d34e4389b28df8323a0
5witchkr <hob4419@gmail.com>
2023-01-26 21:31:01 +0900

    fix: bug

--- src/routes/stage-post/+page.svelte
+++ src/routes/stage-post/+page.svelte
@@ -161,8 +161,7 @@ async function createPost() {
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
         </button>
       </label>
-      <textarea class="textarea" placeholder="Bio" style="text-decoration-color:honeydew;" disabled>
-        {selectedSouls}</textarea>
+      <textarea class="textarea textarea-bordered" placeholder="Bio" readonly> {selectedSouls}</textarea>
 
 
       <div class="divider"></div>
