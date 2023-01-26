efaa9d5ad4d6f9e26aff3f4c41d6ee29efe7c808
5witchkr <hob4419@gmail.com>
2023-01-27 01:43:28 +0900

    fix: bug check

--- src/routes/stage/+page.svelte
+++ src/routes/stage/+page.svelte
@@ -1,6 +1,14 @@
 <script>
   let apidomain = `https://pickban.duckdns.org`;
 
+    //api call values
+    let requestLocations;
+    let requestSteps;
+    let username;
+    let contents;
+    let tacticIdValue;
+    let positionValue;
+
 async function getTactic(){
         let requestLocation = requestLocations.split(" ")[0];
         let requestStep = requestSteps.split(" ")[0];
@@ -70,16 +78,8 @@ async function getTactic(){
         31,32,33,34,35,36,37,38,39,40,
         41,42,43,44,45];
 
+
     let ban;
-
-    //api call values
-    let requestLocations;
-    let requestSteps;
-    let username;
-    let contents;
-    let tacticIdValue;
-    let positionValue;
-
 	let bans = " ";
 
 
